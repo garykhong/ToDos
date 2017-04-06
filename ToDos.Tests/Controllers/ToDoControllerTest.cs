8fbcaf74cff3f095d82591ff809917a990d7bfb1
garykhong <gary_khong@yahoo.com>
2017-04-06 14:09:46 +1000

    Refactor ToDo Controller Details Test

--- ToDos.Tests/Controllers/ToDoControllerTest.cs
+++ ToDos.Tests/Controllers/ToDoControllerTest.cs
@@ -13,6 +13,11 @@ namespace ToDos.Tests.Controllers
     [TestClass]
     public class ToDoControllerTest
     {
+        private FakeToDoDBContext fakeToDoDBContext = new FakeToDoDBContext();
+        private ViewResult toDoDetailsResult;
+        private int toDoID;
+        private string expectedWhatToDo;
+
         [TestMethod]
         public void Index_ViewNameIsIndex()
         {
@@ -29,24 +34,44 @@ namespace ToDos.Tests.Controllers
         [TestMethod]
         public void Details_FirstToDoHasExpectedWhatToDo()
         {
-            FakeToDoDBContext fakeToDoDBContext = new FakeToDoDBContext();
-            fakeToDoDBContext.ToDos.Add(new ToDo { ID = 1, WhatToDo = "Buy Groceries" });
-            ToDoDBContextFactory.SetToDoDBContext(fakeToDoDBContext);
-            ToDoController controller = new ToDoController();
-            ViewResult result = controller.Details(1) as ViewResult;
-            ToDo toDo = (ToDo)result.ViewData.Model;
-            Assert.AreEqual("Buy Groceries", toDo.WhatToDo);
-            Assert.AreEqual("Details", result.ViewName);
+            toDoID = 1;
+            expectedWhatToDo = "Buy Groceries";
+            TestToDoDetailsResult();
         }
 
         [TestMethod]
         public void Details_SecondToDoHasExpectedWhatToDo()
         {
+            toDoID = 2;
+            expectedWhatToDo = "Cook Rice";
+            TestToDoDetailsResult();
+        }
+
+        private void TestToDoDetailsResult()
+        {
+            SetFakeToDoDBContext();
+            SetToDoControllerDetailsResult();
+            AssertToDoDetailsResult();
+        }
+
+        private void SetFakeToDoDBContext()
+        {
+            fakeToDoDBContext.ToDos.Add(new ToDo { ID = 1, WhatToDo = "Buy Groceries" });
+            fakeToDoDBContext.ToDos.Add(new ToDo { ID = 2, WhatToDo = "Cook Rice" });
+            ToDoDBContextFactory.SetToDoDBContext(fakeToDoDBContext);
+        }
+
+        private void SetToDoControllerDetailsResult()
+        {
             ToDoController controller = new ToDoController();
-            ViewResult result = controller.Details(2) as ViewResult;
-            ToDo toDo = (ToDo)result.ViewData.Model;
-            Assert.AreEqual("Cook Rice", toDo.WhatToDo);
-            Assert.AreEqual("Details", result.ViewName);
+            toDoDetailsResult = controller.Details(toDoID) as ViewResult;
+        }
+
+        private void AssertToDoDetailsResult()
+        {
+            ToDo toDo = (ToDo)toDoDetailsResult.ViewData.Model;
+            Assert.AreEqual(expectedWhatToDo, toDo.WhatToDo);
+            Assert.AreEqual("Details", toDoDetailsResult.ViewName);
         }
     }
 }
