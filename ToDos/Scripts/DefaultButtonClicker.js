573dbeeef69c7ca1101986d1840af742910f01eb
garykhong <gary_khong@yahoo.com>
2018-05-03 17:25:39 +1000

    Make what to do a text area for multi line input

--- ToDos/Scripts/DefaultButtonClicker.js
+++ ToDos/Scripts/DefaultButtonClicker.js
@@ -1,9 +1,11 @@
 ﻿
 function SetDefaultButtonToBeClickedOnEnterPress(formID, buttonID) {
     var ButtonKeys = { "EnterKey": 13 };
+    var HtmlTagsToNotButtonClick = { "TextArea": "TEXTAREA" };
     $(function () {
         $("#" + formID).keypress(function (e) {
-            if (e.which == ButtonKeys.EnterKey) {
+            if (e.which == ButtonKeys.EnterKey &&
+                e.target.tagName != HtmlTagsToNotButtonClick.TextArea) {
                 $("#" + buttonID).click();
                 return false;
             }
